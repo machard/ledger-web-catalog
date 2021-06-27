aa2be52768e11fbb2167eb7327e89e7ec303a6c2
machard <matthieu__@hotmail.fr>
2021-06-27 11:14:18 +0200

    readme

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Content from './Content';
 import Developer from "./Developer";
 import Header from './Header';
 import client from "./client";
+import content from "./apps.json";
 
 const styles = (theme: Theme) => createStyles({
   main: {
@@ -20,50 +21,8 @@ const styles = (theme: Theme) => createStyles({
   },
 });
 
-const categories = ["Installed", "Wallet", "Trading", "Borrowing/Lending"];
-
-const apps = [
-  {
-    categories: ["Wallet"],
-    name: "Wallet BTC",
-    description: "Bitcoin (₿) is a decentralized digital currency, without a central bank or single administrator, that can be sent from user to user on the peer-to-peer bitcoin network without the need for intermediaries",
-    image: "https://bitcoin.org/img/icons/opengraph.png?1621851118",
-    icon: "account_balance_wallet",
-    url: "https://www.downloadwebsitetemplates.co.uk/templates/progress/?walletBTC",
-  },
-  {
-    categories: ["Popular", "Wallet"],
-    name: "Wallet Ethereum",
-    description: "Ethereum is a decentralized, open-source blockchain with smart contract functionality. Ether (ETH or Ξ) is the native cryptocurrency of the platform",
-    image: "https://pbs.twimg.com/profile_images/1084788308595617793/DOnqq1OM.jpg",
-    icon: "account_balance_wallet",
-    url: "https://www.downloadwebsitetemplates.co.uk/templates/progress/?walletETH",
-  },
-  {
-    categories: ["Wallet"],
-    name: "Wallet Cosmos",
-    description: "Cosmos (ATOM) is a decentralized ecosystem of independent blockchains that its creator, All In Bits Inc (dba Tendermint Inc), hopes is the foundation for the next generation of internet technology",
-    image: "https://pbs.twimg.com/profile_images/1328431690268729347/flijoJYv_400x400.jpg",
-    icon: "account_balance_wallet",
-    url: "https://www.downloadwebsitetemplates.co.uk/templates/progress/?comsos",
-  },
-  {
-    categories: ["Trading"],
-    name: "Thorswap",
-    description: "THORSwap is the world's first Multichain DEX that utilizes the THORChain network to provide a front-end user interface to perform cross-chain swaps. This is done in a permissionless, trustless, and non-custodial manner",
-    image: "https://pbs.twimg.com/profile_images/1408059226862194698/3k30XVC9_400x400.jpg",
-    icon: "swap_horiz",
-    url: "https://www.downloadwebsitetemplates.co.uk/templates/progress/?thorchain",
-  },
-  {
-    categories: ["Borrowing/Lending"],
-    name: "Liquity",
-    description: "Interest-free liquidity at your fingertips. Borrow LUSD against ETH at 0% interest",
-    image: "https://pbs.twimg.com/profile_images/1247198364707545089/zEgsUkSu_400x400.jpg",
-    icon: "attach_money",
-    url: "https://www.downloadwebsitetemplates.co.uk/templates/progress/?liquity",
-  }
-]
+const apps = content.apps;
+const categories = ["Installed"].concat(content.categories);
 
 export interface PaperbaseProps extends WithStyles<typeof styles> {}
 
